385962e67105baeb7635cb70be30d46d2a8efc70
Roman Z <roman-z@inbox.ru>
2025-06-18 22:19:07 +0500

    Make some changes in src and tests

--- src/product.py
+++ src/product.py
@@ -15,47 +15,36 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
-    # @property
-    # def price(self):
-    #     return self.__price
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        elif new_price < self.__price:
+            user_input = input("Новая цена ниже. Заменить цену?(Y): ")
+            if (
+                user_input == "Y"
+                or user_input == "y"
+                or user_input == "Н"
+                or user_input == "н"
+            ):
+                self.__price = new_price
+            else:
+                return
+        self.__price = new_price
 
     @classmethod
     def new_product(cls, parameters_list: dict):
         product = Product
-        product.name = parameters_list['name']
-        product.description = parameters_list['description']
-        product.price = parameters_list['price']
-        product.quantity = parameters_list['quantity']
+        product.name = parameters_list["name"]
+        product.description = parameters_list["description"]
+        product.price = parameters_list["price"]
+        product.quantity = parameters_list["quantity"]
         return product
-
-
-if __name__ == "__main__":
-    product1 = Product("Xiaomi Redmi Note 12", "1024GB, Черный", "29000", 15)
-    product2 = Product(
-        "Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000, 5
-    )
-
-    # print(product1.name)
-    # print(product1.description)
-    # print(product1.price)
-    # print(product1.quantity)
-
-    product3 = Product.new_product(
-        {'name': 'Samsung Galaxy C23 Ultra', 'description': '256GB, Серый цвет, 200MP камера',
-         'price': 180000, 'quantity': 5}
-    )
-
-    print(product3.name)
-    print(product3.description)
-    print(product3.price)
-    print(product3.quantity)
-
-    # product2.price = 150000
-    #
-    # print(product2.name)
-    # print(product2.description)
-    # print(product2.price)
-    # print(product2.quantity)
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,22 @@
+from src.product import Product
+
 def test_product_init(product):
     assert product.name == "Iphone 15"
     assert product.description == "512GB, Gray space"
     assert product.price == 210000.0
     assert product.quantity == 8
+
+
+def test_product_create():
+    test_product = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    test_product.name = 'Iphone 15'
+    test_product.description = '512GB, Gray space'
+    test_product.price = 210000.0
+    test_product.quantity = 8
+
+
+def test_product_update(capsys, product):
+    product.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == 'Цена не должна быть нулевая или отрицательная'
+
