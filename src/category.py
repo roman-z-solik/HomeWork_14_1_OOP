67c7a6b8a9975ea15b6f90eaf47bf012b18d85da
Roman Z <roman-z@inbox.ru>
2025-06-18 16:38:09 +0500

    Make some changes

--- src/category.py
+++ src/category.py
@@ -1,3 +1,5 @@
+from src.product import Product
+
 class Category:
     """
     Класс Category обладает следующими свойствами:
@@ -15,6 +17,74 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    @property
+    def products(self) -> None:
+        products_str = ''
+        for output_product in self.__products:
+            products_str += (f'{output_product.name}, {output_product.price} руб. Остаток:'
+                             f' {output_product.quantity} шт.\n')
+        return products_str
+
+
+    @products.setter
+    def products(self, new_product: Product):
+        pass
+    #     for product1 in self.__products:
+    #         if new_product == product1:
+    #             print('Есть такой')
+    #             if new_product.price < product1.price:
+    #                 product1.quantity += new_product.quantity
+    #             else:
+    #                 product1.quantity += new_product.quantity
+    #                 product1.price = new_product.price
+    #         else:
+    #             print('Нет такого')
+    #             self.__products.append(product1)
+    #     Category.product_count += 1
+
+
+    def add_product(self, adding_product: Product):
+        self.__products.append(adding_product)
+
+
+
+# if __name__ == '__main__':
+#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+#     # print(product1.name)
+#     # print(product1.description)
+#     # print(product1.price)
+#     # print(product1.quantity)
+#     #
+#     # print(product2.name)
+#     # print(product2.description)
+#     # print(product2.price)
+#     # print(product2.quantity)
+#     #
+#     # print(product3.name)
+#     # print(product3.description)
+#     # print(product3.price)
+#     # print(product3.quantity)
+#
+#     category1 = Category("Смартфоны",
+#                          "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#                          [product1, product2, product3])
+#
+#     # print(category1.name == "Смартфоны")
+#     # print(category1.description)
+#     # print(category1.products)
+#     # print(category1.category_count)
+#     # print(category1.product_count)
+#
+#     category1.products = Product.new_product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера",
+#                             180000, 5)
+#     print(category1.products)
+#     # print(category1.category_count)
+#     # print(category1.product_count)
--- src/product.py
+++ src/product.py
@@ -15,5 +15,51 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    # @classmethod
+    # def new_product(cls, name, description, price, quantity):
+    #     new_product = cls(name, description, price, quantity)
+    #     products = Category.products
+    #     for item in products:
+    #         return new_product
+    #
+    # @property
+    # def price(self):
+    #     return self.__price
+    #
+    #
+    # @price.setter
+    # def price(self, new_price) -> property | None:
+    #     if new_price <= 0:
+    #         print(f'Цена не должна быть нулевая или отрицательная')
+    #     else:
+    #         Product.price = new_price
+
+
+if __name__ == '__main__':
+        product1 = Product('Xiaomi Redmi Note 12', '1024GB, Черный', '29000', 15)
+        product2 = Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера",
+                            180000, 5)
+
+        print(product1.name)
+        print(product1.description)
+        print(product1.price)
+        print(product1.quantity)
+
+        product3 = Product.new_product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера",
+                            180000, 5)
+
+        print(product2.name)
+        print(product2.description)
+        print(product2.price)
+        print(product2.quantity)
+
+        product2.price = 150000
+
+        print(product2.name)
+        print(product2.description)
+        print(product2.price)
+        print(product2.quantity)
