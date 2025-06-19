1dc3723b6333b1ae208879d41b3a7dea95cc63e7
Roman Z <roman-z@inbox.ru>
2025-06-19 13:22:58 +0500

    Make some changes

--- 15.1_main.py
+++ 15.1_main.py
@@ -1,3 +1,6 @@
+from src.product import Product
+from src.category import Category
+
 if __name__ == '__main__':
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
@@ -17,6 +20,6 @@ if __name__ == '__main__':
 
     print(category1.products)
 
-    print(product1 + product2)
-    print(product1 + product3)
-    print(product2 + product3)
+    # print(product1 + product2)
+    # print(product1 + product3)
+    # print(product2 + product3)
--- src/product.py
+++ src/product.py
@@ -48,3 +48,19 @@ class Product:
         product.price = parameters_list["price"]
         product.quantity = parameters_list["quantity"]
         return product
+
+
+    def __str__(self):
+        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
+
+
+if __name__ == '__main__':
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
+
